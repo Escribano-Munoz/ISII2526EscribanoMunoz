5b264317f4125dd5d8c551000450c076b68796b3
RICARDO ESCRIBANO TÁRRAGA <Ricardo.Escribano@alu.uclm.es>
2025-10-03 21:06:39 +0200

    Implementado código de clase Compra.

--- src/AppForSEII2526.API/Models/Compra.cs
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -2,8 +2,51 @@
 {
     public class Compra
     {
-        
+        public Compra()
+        {
 
-       
+        }
+        public Compra()
+        {
+            CompraItems = new List<CompraItem>();
+        }
+
+        public Compra(int id, string nombreCliente, string apellidoCliente, DateTime fechaCompra, string direccionEnvio, List<CompraItem> compraItems, string telefono, string correoElectronico, tiposMetodoPago metodoPago)
+        {
+            Id = id;
+            NombreCliente = nombreCliente;
+            ApellidoCliente = apellidoCliente;
+            FechaCompra = fechaCompra;
+            DireccionEnvio = direccionEnvio;
+            CompraItems = compraItems;
+            Telefono = telefono;
+            CorreoElectronico = correoElectronico;
+            MetodoPago = metodoPago;
+        }
+
+        public int Id { get; set; }
+
+        public string NombreCliente { get;set; }
+
+        public string ApellidoCliente { get; set; }
+
+        public DateTime FechaCompra { get; set; }
+
+        public string DireccionEnvio { get; set; }
+
+        public List<CompraItem> CompraItems { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string CorreoElectronico { get; set; }
+
+
+
+
+        [Display(Name = "Metodo de pago")]
+        [Required]
+        public TiposMetodoPago MetodoPago { get; set; }
+
+
     }
 }
