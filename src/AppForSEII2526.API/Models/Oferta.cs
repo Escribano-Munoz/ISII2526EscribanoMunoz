74692591094b44588e9daff9d55c42e94045cd65
JAVIER MUÑOZ <Javier.Munoz17@alu.uclm.es>
2025-10-03 20:17:15 +0200

    Añadido el código a la clase Oferta

--- src/AppForSEII2526.API/Models/Oferta.cs
+++ src/AppForSEII2526.API/Models/Oferta.cs
@@ -2,5 +2,52 @@
 {
     public class Oferta
     {
+        public DateTime fechaFinal { get; set; }
+        public DateTime fechaInicio { get; set; }
+        public DateTime fechaOferta { get; set; }
+        public int Id { get; set; }
+        public tiposMetodoPago metodoPago { get; set; }
+
+        public tipoSDirigidaOferta parsSocio { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public Oferta()
+        {
+        }
+
+        public Oferta(int id, DateTime fechaInicio, DateTime fechaFinal, DateTime fechaOferta) :
+            this(fechaInicio, fechaFinal, fechaOferta)
+        {
+            Id = id;
+        }
+
+        public Oferta(DateTime fechaInicio, DateTime fechaFinal, DateTime fechaOferta)
+        {
+            this.fechaInicio = fechaInicio;
+            this.fechaFinal = fechaFinal;
+            this.fechaOferta = fechaOferta;
+        }
+    }
+
+    public enum tiposMetodoPago
+    {
+        TarjetaCredito,
+        PayPal,
+        Efectivo
+    }
+
+    public enum tipoSDirigidaOferta
+    {
+        Socios,
+        Clientes
     }
 }
