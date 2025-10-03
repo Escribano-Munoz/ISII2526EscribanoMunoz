c15899ff84380619592f6d1cb2478d8d8a260f44
victoriaescribano <Victoria.Escribano2@alu.uclm.es>
2025-10-03 20:25:32 +0200

    Añadido el código a la clase Reparacion

--- src/AppForSEII2526.API/Models/Reparacion.cs
+++ src/AppForSEII2526.API/Models/Reparacion.cs
@@ -2,6 +2,68 @@
 {
     public class Reparacion
     {
+        public Reparacion()
+        {
 
+        }
+
+        public Reparacion(int id, string nombreCliente, string apellidoCliente, string numTelefono,
+            DateTime fechaRecogida, DateTime fechaEntrega, float precioTotal, tiposMetodosPago metodoPago) :
+            this(nombreCliente, apellidoCliente, numTelefono, fechaRecogida, fechaEntrega, precioTotal, metodoPago)
+        {
+            Id = id;
+        }
+
+        public Reparacion(string nombreCliente, string apellidoCliente, string numTelefono,
+            DateTime fechaRecogida, DateTime fechaEntrega, float precioTotal, tiposMetodosPago metodoPago)
+        {
+            NombreCliente = nombreCliente;
+            ApellidoCliente = apellidoCliente;
+            NumTelefono = numTelefono;
+            FechaRecogida = fechaRecogida;
+            FechaEntrega = fechaEntrega;
+            PrecioTotal = precioTotal;
+        }
+        public int Id { get; set; }
+
+        [Precision(10, 2)]
+        public float PrecioTotal { get; set; }
+
+        public DateTime FechaRecogida { get; set; }
+
+        public DateTime FechaEntrega { get; set; }
+
+        [Display(Name = "Nombre del Cliente")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, ingrese el nombre del cliente")]
+        public string NombreCliente { get; set; }
+
+        [Display(Name = "Apellido del Cliente")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, ingrese el apellido del cliente")]
+        public string ApellidoCliente { get; set; }
+
+        [Display(Name = "Número de Teléfono")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, ingrese el número de teléfono")]
+        public string NumTelefono { get; set; }
+
+        [Display(Name = "Metodo de pago")]
+        [Required]
+        public tiposMetodosPago MetodoPago { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public enum tiposMetodosPago
+        {
+            TarjetaCredito,
+            PayPal,
+            Efectivo
+        }
     }
 }
