962ad3263ff3b5f2d06b44236f59ef86f9f1160f
RICARDO ESCRIBANO TÁRRAGA <Ricardo.Escribano@alu.uclm.es>
2025-10-04 14:32:45 +0200

    Creada Clase CompraItem e implementado su código y añadido más código a la clase Compra.

--- src/AppForSEII2526.API/Models/Compra.cs
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -4,18 +4,23 @@
     {
         public Compra()
         {
-
-        }
-        public Compra()
-        {
             CompraItems = new List<CompraItem>();
         }
 
-        public Compra(int id, string nombreCliente, string apellidoCliente, DateTime fechaCompra, string direccionEnvio, List<CompraItem> compraItems, string telefono, string correoElectronico, tiposMetodoPago metodoPago)
+        public Compra(int id, string nombreCliente, string apellidoCliente, ApplicationUser applicationUser, DateTime fechaCompra, string direccionEnvio, IList<CompraItem> compraItems, string telefono, string correoElectronico, tiposMetodoPago metodoPago) :
+            this(nombreCliente, apellidoCliente,applicationUser, fechaCompra, direccionEnvio, compraItems,telefono, correoElectronico, metodoPago)
         {
             Id = id;
+            
+        }
+
+        public Compra(string nombreCliente, string apellidoCliente, ApplicationUser applicationUser, DateTime fechaCompra, string direccionEnvio, IList<CompraItem> compraItems, string telefono, string correoElectronico, tiposMetodoPago metodoPago)
+        {
+            precioTotal = decimal.Round(compraItems.Sum(pi => pi.Precio * pi.Cantidad), 2);
+
             NombreCliente = nombreCliente;
             ApellidoCliente = apellidoCliente;
+            ApplicationUser = applicationUser;
             FechaCompra = fechaCompra;
             DireccionEnvio = direccionEnvio;
             CompraItems = compraItems;
@@ -24,7 +29,11 @@
             MetodoPago = metodoPago;
         }
 
+
         public int Id { get; set; }
+
+        [Precision(10, 2)]
+        public decimal precioTotal { get; set; }
 
         public string NombreCliente { get;set; }
 
@@ -40,12 +49,26 @@
 
         public string CorreoElectronico { get; set; }
 
-
-
-
         [Display(Name = "Metodo de pago")]
         [Required]
         public TiposMetodoPago MetodoPago { get; set; }
+
+        public enum tiposMetodoPago
+        {
+            TarjetaCredito,
+            PayPal,
+            Efectivo
+        }
+
+         public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
 
 
     }
