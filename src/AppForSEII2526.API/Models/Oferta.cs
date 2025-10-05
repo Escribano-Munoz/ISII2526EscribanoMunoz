e6a0a55afc4e5549062f1dd82f670e8ee4c1adef
JaviiMunozz <Javier.Munoz17@alu.uclm.es>
2025-10-05 20:03:21 +0200

    Terminada tarea 14 (Crear clases Oferta, OfertaItem) (#20)
    * Creada la clase Ofertas
    
    * Añadido el código a la clase Oferta
    
    * Solucionado errores en el codigo.
    
    * Código añadido en la clase Oferta. Cambio en algunas líneas de código en la clase OfertaItem por fallo de mi ordenador.

---
+++ src/AppForSEII2526.API/Models/Oferta.cs
@@ -0,0 +1,57 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Oferta
+    {
+        public DateTime fechaFinal { get; set; }
+        public DateTime fechaInicio { get; set; }
+        public DateTime fechaOferta { get; set; }
+        public int Id { get; set; }
+        public tiposMetodoPago metodoPago { get; set; }
+
+        public tiposDirigidaOferta paraSocio { get; set; }
+
+        public IList<OfertaItem> OfertaItems { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public Oferta()
+        {
+        }
+
+        public Oferta(int id, DateTime fechaInicio, DateTime fechaFinal, DateTime fechaOferta, IList<OfertaItem> OfertaItems) :
+            this(fechaInicio, fechaFinal, fechaOferta,OfertaItems)
+        {
+            Id = id;
+        }
+
+        public Oferta(DateTime fechaInicio, DateTime fechaFinal, DateTime fechaOferta, IList<OfertaItem> ofertaItems)
+        {
+            this.fechaInicio = fechaInicio;
+            this.fechaFinal = fechaFinal;
+            this.fechaOferta = fechaOferta;
+            OfertaItems = ofertaItems;
+        }
+    }
+
+    public enum tiposMetodoPago
+    {
+        TarjetaCredito,
+        PayPal,
+        Efectivo
+    }
+
+    public enum tiposDirigidaOferta
+    {
+        Socios,
+        Clientes
+    }
+
+}
