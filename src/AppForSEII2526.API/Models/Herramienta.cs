f3f7fc7e0e8f8af0cf728b05beb163e4cdc1f9e3
JuanJoseEscribano9 <juanjose.escribano@alu.uclm.es>
2025-10-06 18:21:11 +0200

    16 crear clase compartida herramienta (#23)
    * Creada la clase Alquiler
    
    * Añadido código a la clase Alquiler
    
    * Creada clase AlquilarItem
    
    * Añadido código a la clase AlquilarItem y corregido errores
    
    * Creada clase Herramienta y añadido código para el caso de uso Alquilar
    
    * Corregido codigo herramienta
    
    * Corregido código Herramienta y AlquilarItem

---
+++ src/AppForSEII2526.API/Models/Herramienta.cs
@@ -0,0 +1,63 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Herramienta
+    {
+        public Herramienta()
+        {
+        }
+
+        public Herramienta(Fabricante fabricante, string material, string nombre, float precio)
+        {
+            Fabricante = fabricante;
+            Material = material;
+            Nombre = nombre;
+            Precio = precio;
+        }
+
+        public Herramienta(int id, Fabricante fabricante, string material, string nombre, float precio)
+                : this(fabricante, material, nombre,  precio)
+        {
+            Id = id;
+
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public Fabricante Fabricante { get; set; }
+
+        [Required]
+        public string Material { get; set; }
+        public string Nombre{ get; set; }
+        public int TiempoReparacion { get; set; }
+
+        [Required]
+
+        [Display(Name = "Precio")]
+        public float Precio{ get; set; }
+
+
+        public IList<AlquilarItem> AlquilarItems { get; set; }
+
+        public IList<CompraItem> CompraItems { get; set; }
+
+        public IList<ReparacionItem> ReparacionItems { get; set; }
+
+        public IList<OfertaItem> OfertaItems { get; set; }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Herramienta herramienta &&
+                Fabricante == herramienta.Fabricante &&
+                Id == herramienta.Id &&
+                Precio == herramienta.Precio;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Fabricante, Precio);
+        }
+    }
+}
