d2622b15e29a28595db1fd6d5f588ef22e8a400d
JAVIER MUÑOZ <Javier.Munoz17@alu.uclm.es>
2025-10-06 10:07:39 +0200

    Creada la clase Fabricante. Añadido su código.

---
+++ src/AppForSEII2526.API/Models/Fabricante.cs
@@ -0,0 +1,27 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Fabricante
+    {
+        public Fabricante()
+        {
+        }
+
+        public Fabricante(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public int id { get; set; }
+        public string nombre { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
