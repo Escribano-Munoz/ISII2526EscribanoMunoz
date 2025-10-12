ecf25421d533206d3646dbbdbfecdcf12becf56c
victoriaescribano <133160385+victoriaescribano@users.noreply.github.com>
2025-10-12 20:56:26 +0200

    Modificada clase ApplicationUser y modificados atributos opcionales (#46)

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,33 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+    public ApplicationUser()
+    {
+    }
+    public ApplicationUser(int id, string nombreCliente, string apellidoCliente, string numTelefono, tiposMetodosPago metodoPago)
+    {
+        Id = id;
+        NombreCliente = nombreCliente;
+        ApellidoCliente = apellidoCliente;
+        NumTelefono = numTelefono;
+        MetodoPago = metodoPago;
+    }
+
+    public int Id { get; set; }
+
+    [Display(Name = "Nombre del Cliente")]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, ingrese el nombre del cliente")]
+    public string NombreCliente { get; set; }
+
+    [Display(Name = "Apellido del Cliente")]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, ingrese el apellido del cliente")]
+    public string ApellidoCliente { get; set; }
+
+    [Display(Name = "Número de Teléfono")]
+    public string? NumTelefono { get; set; }
+
+    [Display(Name = "Metodo de pago")]
+    [Required]
+    public tiposMetodosPago MetodoPago { get; set; }
+
 }
