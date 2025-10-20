17bd0a6b6537622823b9be6bbc0a7aa2833453dd
victoriaescribano <Victoria.Escribano2@alu.uclm.es>
2025-10-20 20:04:06 +0200

    Modificado código de la clase HerramientaParaRepararDTO

---
+++ src/AppForSEII2526.API/Controllers/HerramientasController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HerramientasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<HerramientasController> _logger;
+
+        public HerramientasController(ApplicationDbContext context, ILogger<HerramientasController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<Herramienta>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> GetHerramientasParaReparar()
+        {
+            IList<Herramienta> herramientas = await _context.Herramienta
+                .ToListAsync();
+            return Ok(herramientas);
+        }
+
+
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/HerramientaDTOs/HerramientaParaRepararDTO.cs
@@ -0,0 +1,61 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AppForSEII2526.API.DTOs.HerramientaDTOs
+{
+    public class HerramientaParaRepararDTO
+    {
+        public HerramientaParaRepararDTO()
+        {
+        }
+
+        public HerramientaParaRepararDTO(Fabricante fabricante, string material, string nombre, float precio, int tiempoReparacion)
+        {
+            Fabricante = fabricante;
+            Material = material;
+            Nombre = nombre;
+            Precio = precio;
+            TiempoReparacion = tiempoReparacion;
+        }
+
+        public HerramientaParaRepararDTO(int id, Fabricante fabricante, string material, string nombre, float precio, int tiempoReparacion)
+                : this(fabricante, material, nombre, precio, tiempoReparacion)
+        {
+            Id = id;
+
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public Fabricante Fabricante { get; set; }
+
+        [Required]
+        public string Material { get; set; }
+        public string Nombre { get; set; }
+        public int TiempoReparacion { get; set; }
+
+        [Required]
+
+        [Display(Name = "Precio")]
+        public float Precio { get; set; }
+
+
+        public IList<ReparacionItem> ReparacionItems { get; set; }
+
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Herramienta herramienta &&
+                Fabricante == herramienta.Fabricante &&
+                Id == herramienta.Id &&
+                Precio == herramienta.Precio;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Fabricante, Precio);
+        }
+    }
+}
