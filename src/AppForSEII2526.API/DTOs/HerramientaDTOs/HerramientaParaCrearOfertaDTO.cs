cbc23e73a02cc814126ed82fc2d56dd19a43e0cd
JAVIER MUÑOZ <Javier.Munoz17@alu.uclm.es>
2025-10-22 12:43:14 +0200

    Implementado el método Get SELECT para el CU-Crear Ofertas

---
+++ src/AppForSEII2526.API/DTOs/HerramientaDTOs/HerramientaParaCrearOfertaDTO.cs
@@ -0,0 +1,57 @@
+﻿namespace AppForSEII2526.API.DTOs.HerramientaDTOs
+{
+    public class HerramientaParaCrearOfertaDTO
+    {
+        public HerramientaParaCrearOfertaDTO()
+        {
+        }
+
+        public HerramientaParaCrearOfertaDTO(Fabricante fabricante, string material, string nombre, float precio)
+        {
+            Fabricante = fabricante;
+            Material = material;
+            Nombre = nombre;
+            Precio = precio;
+        }
+
+        public HerramientaParaCrearOfertaDTO(int id, Fabricante fabricante, string material, string nombre, float precio)
+                : this(fabricante, material, nombre, precio)
+        {
+            Id = id;
+
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public Fabricante Fabricante { get; set; }
+
+        [Required]
+        public string Material { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Precio")]
+        public float Precio { get; set; }
+
+
+        public IList<OfertaItem> OfertaItems { get; set; }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Herramienta herramienta &&
+                Fabricante == herramienta.Fabricante &&
+                Id == herramienta.Id &&
+                Precio == herramienta.Precio;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Fabricante, Precio);
+        }
+    }
+}
