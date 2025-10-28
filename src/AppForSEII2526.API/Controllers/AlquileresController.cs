bd3497a835ef1733abd8de56cbaf87afb624ef32
JUAN JOSÉ ESCRIBANO TÁRRAGA <JuanJose.Escribano@alu.uclm.es>
2025-10-28 19:52:58 +0100

    Creados DTOs necesarios (AlquilerCreateDTO, AlquilarItemDTO y AlquilerDetailDTO) Añadido AlquileresController y código.

--- src/AppForSEII2526.API/Controllers/AlquileresController.cs
+++ src/AppForSEII2526.API/Controllers/AlquileresController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppForSEII2526.API.DTOs.HerramientaDTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppForSEII2526.API.Controllers
@@ -7,5 +8,54 @@ namespace AppForSEII2526.API.Controllers
     [ApiController]
     public class AlquileresController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        private readonly ILogger<HerramientasController> _logger;
+
+        public AlquileresController(ApplicationDbContext context, ILogger<HerramientasController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(RentalDetailDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetRental(int id)
+        {
+            if (_context.Rentals == null)
+            {
+                _logger.LogError("Error: Rentals table does not exist");
+                return NotFound();
+            }
+
+            var rental = await _context.Rentals
+             .Where(r => r.Id == id)
+                 .Include(r => r.RentalItems) //join table RentalItems
+                    .ThenInclude(ri => ri.Movie) //then join table Movies
+                        .ThenInclude(movie => movie.Genre) //then join table Genre
+             .Select(r => new RentalDetailDTO(r.Id, r.RentalDate, r.CustomerUserName,
+                    r.CustomerNameSurname, r.DeliveryAddress,
+                    (PaymentMethodTypes)r.PaymentMethod,
+                    r.RentalDateFrom, r.RentalDateTo,
+                    r.RentalItems
+                        .Select(ri => new RentalItemDTO(ri.Movie.Id,
+                                ri.Movie.Title, ri.Movie.Genre.Name,
+                                ri.Movie.PriceForRenting, ri.Description)).ToList<RentalItemDTO>()))
+             .FirstOrDefaultAsync();
+
+
+            if (rental == null)
+            {
+                _logger.LogError($"Error: Rental with id {id} does not exist");
+                return NotFound();
+            }
+
+
+            return Ok(rental);
+        }
+
+
     }
 }
---
+++ src/AppForSEII2526.API/DTOs/AlquilerDTOs/AlquilerCreateDTO.cs
@@ -0,0 +1,76 @@
+﻿namespace AppForSEII2526.API.DTOs.AlquilerDTOs
+{
+    public class AlquilerCreateDTO
+    {
+        public AlquilerCreateDTO(string nombreCliente, string apellidoCliente, string direccionEnvio, DateTime fechaInicio, DateTime fechaFin, IList<AlquilarItemDTO> alquilarItems)
+        {
+            NombreCliente = nombreCliente ?? throw new ArgumentNullException(nameof(nombreCliente));
+            ApellidoCliente = apellidoCliente ?? throw new ArgumentNullException(nameof(apellidoCliente));
+            DireccionEnvio = direccionEnvio ?? throw new ArgumentNullException(nameof(direccionEnvio));
+            FechaInicio = fechaInicio;
+            FechaFin = fechaFin;
+            AlquilarItems = alquilarItems ?? throw new ArgumentNullException(nameof(alquilarItems));
+        }
+
+        public AlquilerCreateDTO()
+        {
+            AlquilarItems = new List<AlquilarItemDTO>();
+        }
+
+        public DateTime FechaInicio { get; set; }
+
+        public DateTime FechaFin { get; set; }
+
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.MultilineText)]
+        [Display(Name = "Direccion de Envío")]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "La direccion de envio debe tener al menos 10 caracteres")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, introduce tu direccion para el envio")]
+        public string DireccionEnvio { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string NombreCliente { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, pon tu nombre y apellidos")]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "Nombre y apellidos debe contener al menos 10 caracteres")]
+        public string ApellidoCliente { get; set; }
+
+        public IList<AlquilarItemDTO> AlquilarItems { get; set; }
+
+        private int NumeroDeDias
+        {
+            get
+            {
+                return (FechaFin - FechaInicio).Days;
+            }
+        }
+
+        [Display(Name = "Precio Total")]
+        [JsonPropertyName("PrecioTotal")]
+        public double PrecioTotal
+        {
+            get
+            {
+                return AlquilarItems.Sum(ai => ai.Precio * NumeroDeDias);
+            }
+        }
+
+        protected bool CompareDate(DateTime date1, DateTime date2)
+        {
+            return (date1.Subtract(date2) < new TimeSpan(0, 1, 0));
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is AlquilerCreateDTO dTO &&
+                   CompareDate(FechaInicio, dTO.FechaInicio) &&
+                   CompareDate(FechaFin, dTO.FechaFin) &&
+                   DireccionEnvio == dTO.DireccionEnvio &&
+                   NombreCliente == dTO.NombreCliente &&
+                   ApellidoCliente == dTO.ApellidoCliente &&
+                   AlquilarItems.SequenceEqual(dTO.AlquilarItems) &&
+                   PrecioTotal == dTO.PrecioTotal;
+        }
+    }
+}
