d1a7737973acda94650c2f0eb72d9c7d165f0665
RicardoET999 <ricardo.escribano@alu.uclm.es>
2025-10-28 17:55:45 +0100

    Implementado el método Get SELECT para el CU-Comprar Herramientas (#73)
    * Crear HerramientasController con primeros métodos de prueba y carpeta para DTOs.
    
    * Añadido código a HerramientasParaComprarDTO.
    
    * Implementado el método GET Select para el CU-Comprar Herramientas.
    
    * Eliminada Migración para pull request.
    
    * Modificado código de HerramientaController.

--- src/AppForSEII2526.API/Controllers/HerramientasController.cs
+++ src/AppForSEII2526.API/Controllers/HerramientasController.cs
@@ -8,18 +8,34 @@ namespace AppForSEII2526.API.Controllers
     [ApiController]
     public class HerramientasController : ControllerBase
     {
-
         private readonly ApplicationDbContext _context;
 
         private readonly ILogger<HerramientasController> _logger;
 
-        public HerramientasController(ApplicationDbContext context,
-            ILogger<HerramientasController> logger)
+        public HerramientasController(ApplicationDbContext context, ILogger<HerramientasController> logger)
         {
             _context = context;
             _logger = logger;
         }
 
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<HerramientaParaComprarDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetHerramientasParaComprar(string? nombre, string? material, string?fabricante, float? precio)
+        {
+            var herramientas = await _context.Herramienta
+                .Include(h => h.Fabricante)
+                .Include(h => h.CompraItems)
+                    .ThenInclude(ci => ci.Compra)
+                .Where(h => ((h.Nombre.Contains(nombre)) || (nombre == null))
+                    && ((h.Fabricante.nombre.Equals(fabricante)) || (fabricante == null))
+                    && ((h.Material.Equals(material)) || (fabricante == null))
+                    && ((h.Precio.Equals(precio)) || (precio == null)))
+                .OrderBy(h => h.Nombre)
+                .Select(h => new HerramientaParaComprarDTO(
+                    h.Fabricante,
+                    h.Material,
+                    h.Nombre,
 
         [HttpGet]
         [Route("[action]")]
@@ -45,6 +61,5 @@ namespace AppForSEII2526.API.Controllers
 
             return Ok(herramientas);
         }
-
     }
 }
