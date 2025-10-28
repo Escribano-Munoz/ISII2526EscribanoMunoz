09081db31aac231b1a10f8743ea5f4a34fcdecd1
JUAN JOSÉ ESCRIBANO TÁRRAGA <JuanJose.Escribano@alu.uclm.es>
2025-10-28 20:18:21 +0100

    Modificado codigo del get details en AlquileresController

--- src/AppForSEII2526.API/Controllers/AlquileresController.cs
+++ src/AppForSEII2526.API/Controllers/AlquileresController.cs
@@ -1,4 +1,5 @@
-﻿using AppForSEII2526.API.DTOs.HerramientaDTOs;
+﻿using AppForSEII2526.API.DTOs.AlquilerDTOs;
+using AppForSEII2526.API.DTOs.HerramientaDTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,9 +11,9 @@ namespace AppForSEII2526.API.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        private readonly ILogger<HerramientasController> _logger;
+        private readonly ILogger<AlquileresController> _logger;
 
-        public AlquileresController(ApplicationDbContext context, ILogger<HerramientasController> logger)
+        public AlquileresController(ApplicationDbContext context, ILogger<AlquileresController> logger)
         {
             _context = context;
             _logger = logger;
@@ -20,40 +21,39 @@ namespace AppForSEII2526.API.Controllers
 
         [HttpGet]
         [Route("[action]")]
-        [ProducesResponseType(typeof(RentalDetailDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(AlquilerDetailDTO), (int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        public async Task<ActionResult> GetRental(int id)
+        public async Task<ActionResult> GetAlquilerDetail(int id)
         {
-            if (_context.Rentals == null)
+            if (_context.Alquiler == null)
             {
-                _logger.LogError("Error: Rentals table does not exist");
+                _logger.LogError("Error: Alquileres table does not exist");
                 return NotFound();
             }
 
-            var rental = await _context.Rentals
-             .Where(r => r.Id == id)
-                 .Include(r => r.RentalItems) //join table RentalItems
-                    .ThenInclude(ri => ri.Movie) //then join table Movies
-                        .ThenInclude(movie => movie.Genre) //then join table Genre
-             .Select(r => new RentalDetailDTO(r.Id, r.RentalDate, r.CustomerUserName,
-                    r.CustomerNameSurname, r.DeliveryAddress,
-                    (PaymentMethodTypes)r.PaymentMethod,
-                    r.RentalDateFrom, r.RentalDateTo,
-                    r.RentalItems
-                        .Select(ri => new RentalItemDTO(ri.Movie.Id,
-                                ri.Movie.Title, ri.Movie.Genre.Name,
-                                ri.Movie.PriceForRenting, ri.Description)).ToList<RentalItemDTO>()))
+            var alquiler = await _context.Alquiler
+             .Where(a => a.Id == id)
+                 .Include(a => a.AlquilarItems) //join table AlquilarItems
+                    .ThenInclude(ai => ai.Herramienta) //then join table Herramientas
+                        .ThenInclude(herramienta => herramienta.Fabricante) //then join table Fabricante
+             .Select(a => new AlquilerDetailDTO(a.Id, a.FechaAlquiler, a.ApplicationUser.NombreCliente,
+                    a.ApplicationUser.ApellidoCliente, a.ApplicationUser.DireccionEnvio,
+                    a.FechaInicio, a.FechaFin,
+                    a.AlquilarItems
+                        .Select(ai => new AlquilarItemDTO(ai.Herramienta.Id,
+                                ai.Herramienta.Nombre, ai.Herramienta.Material,
+                                ai.Herramienta.Precio, ai.Cantidad)).ToList<AlquilarItemDTO>()))
              .FirstOrDefaultAsync();
 
 
-            if (rental == null)
+            if (alquiler == null)
             {
-                _logger.LogError($"Error: Rental with id {id} does not exist");
+                _logger.LogError($"Error: Alquiler with id {id} does not exist");
                 return NotFound();
             }
 
 
-            return Ok(rental);
+            return Ok(alquiler);
         }
 
 
