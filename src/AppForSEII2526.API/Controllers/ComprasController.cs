3dd7414e9df294e38db734ca9687cfddd709fa2d
RICARDO ESCRIBANO TÁRRAGA <Ricardo.Escribano@alu.uclm.es>
2025-11-03 13:10:05 +0100

    Implementado y corregido errores del GetDetail y creados los DTOs necesarios.

--- src/AppForSEII2526.API/Controllers/ComprasController.cs
+++ src/AppForSEII2526.API/Controllers/ComprasController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppForSEII2526.API.DTOs.CompraDTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppForSEII2526.API.Controllers
@@ -7,5 +8,51 @@ namespace AppForSEII2526.API.Controllers
     [ApiController]
     public class ComprasController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<ComprasController> _logger;
+
+        public ComprasController(ApplicationDbContext context, ILogger<ComprasController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(CompraDetailDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetCompraDetail(int id)
+        {
+            if (_context.Compra == null)
+            {
+                _logger.LogError("Error: Compra table does not exist");
+                return NotFound();
+            }
+
+            var compra = await _context.Compra
+             .Where(c => c.Id == id)
+                 .Include(c => c.CompraItems) //join table RentalItems
+                    .ThenInclude(ci => ci.Herramienta) //then join table Movies
+                        .ThenInclude(herramienta => herramienta.Fabricante) //then join table Genre
+             .Select(c => new CompraDetailDTO(c.ApplicationUser.Id, c.ApplicationUser.NombreCliente,
+                    c.ApplicationUser.ApellidoCliente, c.ApplicationUser.DireccionEnvio, c.FechaCompra,
+                    c.CompraItems
+                        .Select(ci => new CompraItemDTO(ci.Herramienta.Id,
+                                ci.Herramienta.Nombre, ci.Herramienta.Material,
+                                ci.Herramienta.Precio, ci.Cantidad, ci.Descripcion)).ToList<CompraItemDTO>()))
+             .FirstOrDefaultAsync();
+
+
+            if (compra == null)
+            {
+                _logger.LogError($"Error: Compra with id {id} does not exist");
+                return NotFound();
+            }
+
+
+            return Ok(compra);
+        }
+
+       
     }
 }
---
+++ src/AppForSEII2526.API/DTOs/CompraDTOs/CompraDetailDTO.cs
@@ -0,0 +1,34 @@
+﻿namespace AppForSEII2526.API.DTOs.CompraDTOs
+{
+    public class CompraDetailDTO : CompraCreateDTO
+    {
+        public CompraDetailDTO(int id, string nombreCliente, string apellidoCliente,
+            string direccionEnvio, DateTime fechaCompra, IList<CompraItemDTO> compraItems)
+            : base(
+                   nombreCliente,
+                   apellidoCliente,
+                   direccionEnvio,
+                   compraItems)
+        {
+            Id = id;
+            FechaCompra = fechaCompra;
+        }
+        public int Id { get; set; }
+
+        public DateTime FechaCompra { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CompraDetailDTO dTO &&
+                   base.Equals(obj) &&
+                   PrecioTotal == dTO.PrecioTotal &&
+                   Id == dTO.Id &&
+                   CompareDate(FechaCompra, dTO.FechaCompra);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Id, FechaCompra);
+        }
+    }
+}
