4364f5963359cca3edddfe7b3887665dafaded54
victoriaescribano <133160385+victoriaescribano@users.noreply.github.com>
2025-11-05 12:06:40 +0100

    Implementado el método GET details para el CU-RepararHerramientas (#81)
    * Creado controlador ReparacionesController
    
    * Implementado el método GET details para el CU-RepararHerramientas

---
+++ src/AppForSEII2526.API/Controllers/ReparacionesController.cs
@@ -0,0 +1,56 @@
+﻿using AppForSEII2526.API.DTOs.ReparacionDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReparacionesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<ReparacionesController> _logger;
+
+        public ReparacionesController(ApplicationDbContext context, ILogger<ReparacionesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(ReparacionDetailDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetReparacionDetail(int id)
+        {
+            if (_context.Reparacion == null)
+            {
+                _logger.LogError("Error: Reparaciones table does not exist");
+                return NotFound();
+            }
+
+            var reparacion = await _context.Reparacion
+             .Where(r => r.Id == id)
+                 .Include(r => r.ReparacionItems)
+                    .ThenInclude(ri => ri.Herramienta)
+                        .ThenInclude(herramienta => herramienta.Fabricante)
+             .Select(r => new ReparacionDetailDTO(r.Id, r.ApplicationUser.NombreCliente, r.ApplicationUser.ApellidoCliente,
+                    r.FechaRecogida, r.FechaEntrega,
+                    r.ReparacionItems
+                        .Select(ri => new ReparacionItemDTO(ri.Herramienta.Id,
+                                ri.Herramienta.Nombre, ri.Herramienta.Precio,
+                                ri.cantidad, ri.descripcion)).ToList<ReparacionItemDTO>()))
+             .FirstOrDefaultAsync();
+
+
+            if (reparacion == null)
+            {
+                _logger.LogError($"Error: Reparacion with id {id} does not exist");
+                return NotFound();
+            }
+
+
+            return Ok(reparacion);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/ReparacionDTOs/ReparacionDetailDTO.cs
@@ -0,0 +1,33 @@
+﻿namespace AppForSEII2526.API.DTOs.ReparacionDTOs
+{
+    public class ReparacionDetailDTO : ReparacionCreateDTO
+    {
+        public ReparacionDetailDTO(int id, string nombreCliente, string apellidoCliente, DateTime fechaRecogida, 
+            DateTime fechaEntrega, IList<ReparacionItemDTO> reparacionItems)
+            : base(
+                   nombreCliente,
+                   apellidoCliente,
+                   fechaRecogida,
+                   fechaEntrega,
+                   reparacionItems
+                  )
+        {
+            Id = id;
+        }
+        public int Id { get; set; }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ReparacionDetailDTO dTO &&
+                   base.Equals(obj) &&
+                   PrecioTotal == dTO.PrecioTotal &&
+                   Id == dTO.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Id);
+        }
+    }
+}
